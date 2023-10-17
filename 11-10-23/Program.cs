20cebd1d07578dcaa72fae650f6587b97e6a3826
Łukasz Stępień <lukaszatepien@gmail.com>
2023-10-17 20:32:11 +0200

    Done ex from 11-10-23

---
+++ 11-10-23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[4];
+        Console.WriteLine("Podaj liczby X Y A B");
+
+        for (int i = 0; i < 4; i++)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                numbers[i] = number;
+            }
+        }
+
+        for (int i = 0; i < numbers[1]; i++)
+        {
+            Console.WriteLine("");
+        }
+
+        for (int i = 0; i < numbers[3]; i++)
+        {
+            for (int j = 0; j < numbers[0]; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < numbers[2]; j++)
+            {
+                Console.Write("x");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
